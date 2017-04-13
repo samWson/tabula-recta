1212f2bbb18f257b3a0ff48b6f8d8b817bc5c45e
Samuel Wilson <samWson@users.noreply.github.com>
2017-04-14 10:34:31 +1200

    Add keywordize function

--- src/tabula_recta/core.clj
+++ src/tabula_recta/core.clj
@@ -2,12 +2,22 @@
   (:gen-class))
 
 (defn alphabet
-  "Returns a lazy sequence of alphabet characters"
-  []
-  (map char
-       (range (int \A) (inc (int \Z)))))
+  "Returns a lazy sequence of alphabet string characters"
+  []  
+  (map str
+    (map char
+       (range (int \A) (inc (int \Z))))))
+
+(defn keywordize
+  "Returns a list of keywords"
+  [collection]
+  (map keyword (doall (collection))))
+
+;; Trying to create the first row of the tabula-recta here
+(def first-row (zipmap (keywordize alphabet) (zipmap (keywordize alphabet) (alphabet))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
