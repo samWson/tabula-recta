84aeeef3f809ca2c222405a73c02283e450a375a
Samuel Wilson <samWson@users.noreply.github.com>
2017-04-15 17:41:50 +1200

    Refactor alphabet function into a constant

--- src/tabula_recta/core.clj
+++ src/tabula_recta/core.clj
@@ -1,12 +1,7 @@
 (ns tabula-recta.core
   (:gen-class))
 
-(defn alphabet
-  "Returns a lazy sequence of alphabet string characters"
-  []  
-  (map str
-    (map char
-       (range (int \A) (inc (int \Z))))))
+(def alphabet (map char (range (int \A) (inc (int \Z)))))
 
 (defn keywordize
   "Returns a list of keywords"
@@ -15,6 +10,14 @@
 
 ;; Trying to create the first row of the tabula-recta here
 (def first-row (zipmap (keywordize alphabet) (zipmap (keywordize alphabet) (alphabet))))
+
+(def row {:A {:A "A" :B "B" :C "C"}})
+
+(get-in row [:A :C])
+
+(alphabet)
+
+(keywordize alphabet)
 
 (defn -main
   "I don't do a whole lot ... yet."
