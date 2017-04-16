fb869d94d4a36cc7143f035e01add206c9b2b899
Samuel Wilson <samWson@users.noreply.github.com>
2017-04-16 21:44:18 +1200

    Refactor test-alphabet, add test-tabula-recta
    The alphabet function is redefined as a constant. The test structure is refactored to be synatically correct after this change. New tests added for tabula-recta lookup functionality.

--- test/tabula_recta/core_test.clj
+++ test/tabula_recta/core_test.clj
@@ -7,5 +7,11 @@
 
 (deftest test-alphabet
   (testing "Alphabet generation"
-    (is (= (take 3 (cycle (alphabet))) '(\A \B \C)))
-    (is (= (take 29 (cycle (alphabet))) '(\A \B \C \D \E \F \G \H \I \J \K \L \M \N \O \P \Q \R \S \T \U \V \W \X \Y \Z \A \B \C)))))
+    (is (= (take 3 (cycle alphabet)) '(\A \B \C)))
+    (is (= (take 29 (cycle alphabet)) '(\A \B \C \D \E \F \G \H \I \J \K \L \M \N \O \P \Q \R \S \T \U \V \W \X \Y \Z \A \B \C)))))
+
+(deftest test-tabula-recta
+  (testing "Tabula lookup"
+    (is (= (get-in tabula-recta [:M :N]) \Z))
+    (is (= (get-in tabula-recta [:A :A]) \A))
+    (is (= (get-in tabula-recta [:Z :Z]) \Y))))
