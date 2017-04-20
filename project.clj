069ec33c9e8d93af5122971ed45496a2da2905e3
Samuel <samWson@users.noreply.github.com>
2017-04-20 21:00:55 +1200

    Change license attributes to the MIT license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject tabula-recta "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "GNU General Public License Version 3"
-            :url "https://www.gnu.org/licenses/gpl.html"}
+  :license {:name "MIT License"
+            :url "https://mit-license.org/"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot tabula-recta.core
   :target-path "target/%s"
