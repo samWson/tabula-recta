3d86c96c8e0ce6a49a0bee31becad21d97cd6393
Samuel Wilson <samWson@users.noreply.github.com>
2017-04-21 20:41:34 +1200

    Remove commented out code

--- test/tabula_recta/core_test.clj
+++ test/tabula_recta/core_test.clj
@@ -2,9 +2,6 @@
   (:require [clojure.test :refer :all]
             [tabula-recta.core :refer :all]))
 
-;(def 3-chars (\A \B \C))
-;(def 29-chars [\A \B \C \D \E \F \G \H \I \J \K \L \M \N \O \P \Q \R \S \T \U \V \W \X \Y \Z \A \B \C])
-
 (deftest test-alphabet
   (testing "Alphabet generation"
     (is (= (take 3 (cycle alphabet)) '(\A \B \C)))
